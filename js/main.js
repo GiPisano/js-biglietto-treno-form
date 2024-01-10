a9b4f973525e119a4036242e342cdadcb26cdfea
GiPisano <pisanogiacomo9@gmail.com>
2024-01-10 23:34:23 +0100

    add ticket generator

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 const btnResult = document.getElementById('button-result');
+const btnReset = document.getElementById('button-reset');
 const userName = document.getElementById('user-name');
 const userKm = document.getElementById('user-km');
 const userAge = document.getElementById('user-age');
+const textName = document.getElementById('text-name');
+const ticketPrice = document.getElementById('ticket-price');
+const carriageNumber = document.getElementById('carriage-number');
+const cpCode = document.getElementById('codice-cp');
+const ticketTitle = document.querySelector('.ticket-title')
+const ticket = document.querySelector('.ticket-container');
+const bannerTitle = document.querySelector('.banner');
 
 const price = 0.21;
 
+// percentage
 let underAgePercentage = 20;
 let overAgePercentage = 40;
 
@@ -22,10 +31,10 @@ console.log(underAgePrice);
 
 // ticket price overage
 const overAgePrice = price - overAgeDiscount;
-console.log(overAgePrice);
+console.log(overAgePrice); 
 
-// final price 
 
+// button Genera
 
 btnResult.addEventListener('click', function(){
 
@@ -34,27 +43,43 @@ btnResult.addEventListener('click', function(){
 
     if(!isNaN(km)){
 
+        ticket.style.display = "flex";
+        ticketTitle.style.display = "block"
+        textName.innerText = userName.value;
+        carriageNumber.innerText = Math.floor(Math.random() * 10) +1;
+        cpCode.innerText = Math.floor(Math.random() * 100000) + 1;;
+
         let underAgeCost = underAgePrice * km;
         let overAgeCost = overAgePrice * km;
-        let tiketPriceCost = price * km;
+        let ticketPriceCost = price * km;
 
         if(age == 'underage'){
+            ticketPrice.innerText = underAgeCost.toFixed(2) + '€'
             console.log(underAgeCost)
         } else if(age == 'overage'){
+            ticketPrice.innerText = overAgeCost.toFixed(2) + '€'
             console.log(overAgeCost)
         }else{
+            ticketPrice.innerText = ticketPriceCost.toFixed(2) + '€'
             console.log(tiketPriceCost)
         }
     
     
     }
     else{
-        console.log("riprova");
+        bannerTitle.innerText = 'I dati inseriti non sono corretti, si prega di riprovare'
         }
     }
 
    
 )
 
+
+// button Reset
+btnReset.addEventListener('click', function(){
+    ticketTitle.style.display = "none";
+    ticket.style.display = "none";
+    bannerTitle.style.display = "none";
+})
 
 
